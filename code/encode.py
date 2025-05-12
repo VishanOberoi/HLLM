023d3db868217b81dc4d1914475d81ddc309e62e
JingyuanHe1222 <jingyuanhe1222@gmail.com>
2025-05-11 22:04:39 -0400

    item encoding

---
+++ code/encode.py
@@ -0,0 +1,369 @@
+import os
+import numpy as np
+import argparse
+from tqdm import tqdm 
+import torch
+import pickle
+import sys
+
+from cProfile import run
+from logging import getLogger
+import torch
+import json
+
+import torch.distributed as dist
+from torch.utils.data import Dataset, DataLoader
+
+from transformers import AutoTokenizer
+
+from REC.config import Config
+from REC.utils import init_logger, get_model, init_seed, set_color
+from REC.utils.ClueWeb22Api import ClueWeb22Api, create_shards
+
+from REC.data import *
+from REC.data.dataset.collate_fn import customize_rmpad_collate
+from REC.data.dataset import BatchTextDataset
+
+from REC.trainer import Trainer
+
+
+import lightning as L
+from lightning.fabric.strategies import DeepSpeedStrategy, DDPStrategy
+
+
+
+
+class ClueWebBatchTextDataset(Dataset):
+    """
+    ClueWeb dataset for item encoding. 
+    """
+
+    def __init__(self, config, args):
+
+        self.args = args
+
+        # ClueWeb22 location on disk 
+        self.dataset_dir = "/data/datasets/clueweb22/ClueWeb22_B"
+
+        # get the cwid - id map 
+        self.id_to_cwid = {}
+        # the list of internal item ids 
+        self.item_list = [] 
+        with open(self.args.id_map_path, "r") as f:
+            for line in f:  
+                parts = line.strip().split("\t")
+                self.id_to_cwid[int(parts[1])] = parts[0]
+                # the encode data is the internal ids 
+                self.item_list.append(int(parts[1]))
+   
+        if self.args.dataset_number_of_shards > 1:
+            self.item_list = create_shards(
+                data=self.item_list, 
+                num_shards=self.args.dataset_number_of_shards, 
+                index=self.args.dataset_shard_index
+            )
+        print(f"EncodeDataset_ClueWeb22 shard {self.args.dataset_shard_index} length: {len(self.item_list)}")
+
+        self.item_num = len(self.id_to_cwid) # total number of items 
+        self.max_text_length = config['MAX_TEXT_LENGTH']
+        self.device = config['device']
+        self.text_keys = config['text_keys']
+        self.tokenizer = AutoTokenizer.from_pretrained(config['item_pretrain_dir'], trust_remote_code=True)
+
+        self.item_prompt = config['item_prompt']
+        self.item_emb_token_n = config['item_emb_token_n']
+
+
+    def __len__(self):
+        # the current shard 
+        return len(self.item_list)
+        
+
+    def __getitem__(self, index):
+
+        def get_features(index): 
+            # extract required webpage contents from ClueWeb22Api
+            clueweb_api = ClueWeb22Api(cweb_doc_id, self.dataset_dir)
+            clean_txt = eval(clueweb_api.get_clean_text())
+            content = clean_txt["Clean-Text"]
+            title = content.split('\n')[0].replace("\n", "").replace("\t", "").replace("\r", "").replace("\'", "").replace("\"", "").strip()
+            content = content.replace("\n", "").replace("\t", "").replace("\r", "").replace("\'", "").replace("\"", "").strip()
+            item_feature = {
+                "title": title, 
+                "description": content
+            }
+            if 'tag' in self.text_keys: 
+                topics = ",".join(clueweb_api.get_topics()) 
+                item_feature['tag'] = topics
+            return item_feature
+
+        def process_item(cweb_doc_id):
+            # tokenize webpage 
+            item_i = get_features(cweb_doc_id)
+            text_str = ""
+            if len(item_i):
+                text_str = f"{self.item_prompt}"
+                for key in self.text_keys: # ['title', 'tag', 'description']
+                    value = item_i[key]
+                    if value and str(value) != 'nan':
+                        text_str += f"{key}: {value}"
+                        
+            ids = self.tokenizer.encode(text_str)
+            ids = ids[:self.max_text_length]
+            mask = [1] * len(ids)
+            return ids, mask
+
+        # document processing 
+        id_ = self.item_list[index] # internal_cwid 
+        # get the corresponing cwid 
+        cweb_doc_id = self.id_to_cwid[id_] 
+
+        # get the tokenized ids
+        pos_input_ids, pos_cu_input_lens, pos_position_ids = [], [], []
+        ids, _ = process_item(cweb_doc_id)
+
+        # flash attention prep 
+        pos_input_ids.extend(ids + [0] * self.item_emb_token_n)
+        pos_cu_input_lens.append(len(ids) + self.item_emb_token_n)
+        pos_position_ids.extend((torch.arange(len(ids) + self.item_emb_token_n) + (self.max_text_length - len(ids))).tolist())
+        outputs = {
+            "pos_item_ids": torch.as_tensor(index, dtype=torch.int64),
+            "pos_input_ids": torch.as_tensor(pos_input_ids, dtype=torch.int64),
+            "pos_cu_input_lens": torch.as_tensor(pos_cu_input_lens, dtype=torch.int64),
+            "pos_position_ids": torch.as_tensor(pos_position_ids, dtype=torch.int64)
+        }
+
+        return outputs
+
+
+
+
+def convert_str(s):
+    try:
+        if s.lower() == 'none':
+            return None
+        if s.lower() == 'true':
+            return True
+        if s.lower() == 'false':
+            return False
+        float_val = float(s)
+        if float_val.is_integer():
+            return int(float_val)
+        return float_val
+    except ValueError:
+        print(f"Unable to convert the string '{s}' to None / Bool / Float / Int, retaining the original string.")
+        return s
+
+
+def to_device(data):
+    device = "cuda"
+    if isinstance(data, tuple) or isinstance(data, list):
+        tdata = ()
+        for d in data:
+            d = d.to(device)
+            tdata += (d,)
+        return tdata
+    elif isinstance(data, dict):
+        for k, v in data.items():
+            data[k] = v.to(device)
+        return data
+    else:
+        return data.to(device)
+
+
+def item_encode(model, config, args, output_path, save_step=50): 
+
+    item_data = ClueWebBatchTextDataset(config, args)
+    # item_batch_size = config['MAX_ITEM_LIST_LENGTH'] * config['train_batch_size']
+    item_batch_size = args.batch_size
+    item_loader = DataLoader(item_data, batch_size=item_batch_size, num_workers=args.num_workers, shuffle=False, pin_memory=True, collate_fn=customize_rmpad_collate)
+    print(f"Inference item_data with {item_batch_size = } {len(item_loader) = }")
+    
+    # ckpt file 
+    batch_output_path = output_path + ".temp"
+
+    # checkpoint resumption 
+    if os.path.exists(batch_output_path):
+        with open(batch_output_path, 'rb') as f:
+            item_feature, start_from_idx = pickle.load(f)
+            print(f"loaded checkpoint from batch {start_from_idx}")
+            print(f"checkpoint contains {len(item_feature)} batches")
+    else: 
+        item_feature = []
+        start_from_idx = -1
+
+    print(f"Start encoding from batch {start_from_idx+1}")
+    model.eval()
+    with torch.no_grad():
+        for idx, items in tqdm(enumerate(item_loader), total=len(item_loader)):
+
+            # skip the finished batches 
+            if idx <= start_from_idx: 
+                continue 
+
+            items = to_device(items)
+            items = model(items, mode='compute_item')
+            item_feature.append(items.cpu().numpy())
+
+            # checkpoint resumption 
+            if idx % save_step == 0: 
+                # first save to temp temp location 
+                with open(f"{batch_output_path}_1", 'wb') as f:
+                    pickle.dump((item_feature, idx), f)
+                print(f"saved checkpoint at: {output_path}.temp at batch {idx}")
+                sys.stdout.flush()
+                # avoid preemption when saving ckpt...
+                os.rename(f"{batch_output_path}_1", batch_output_path)
+
+        if isinstance(items, tuple):
+            item_feature = torch.cat([x[0] for x in self.item_feature]), torch.cat([x[1] for x in self.item_feature])
+        else:
+            item_feature = torch.cat(item_feature)
+    item_feature = item_feature.numpy()
+    # output embeddings 
+    with open(output_path, 'wb') as f:
+        pickle.dump(item_feature, f)
+
+
+
+def seq_encode(model, config, args, output_path, save_step=50): 
+
+    seq_loader = None # TODO 
+    seq_emb = []
+    with torch.no_grad():
+        for idx, seqs in tqdm(enumerate(seq_loader), total=len(seq_loader)):
+            attention_mask = (item_seq > 0).int()
+            pos_embedding = item_feature[seqs] # historically interacted 
+            user_embedding = self.user_llm(inputs_embeds=pos_embedding, attention_mask=attention_mask).hidden_states[-1]
+            seq_output = user_embedding[:, -1]
+            seq_emb.append(seq_output)
+    seq_emb = torch.cat(seq_emb).numpy()
+    # output embeddings 
+    with open(output_path, 'wb') as f:
+        pickle.dump(seq_emb, f)
+
+
+
+def run(config_file=None, extra_args=[], custom_args=None):
+
+    # configurations initialization
+    config = Config(config_file_list=config_file)
+
+    device = torch.device("cuda")
+    config['device'] = device
+    if len(extra_args):
+        for i in range(0, len(extra_args), 2):
+            key = extra_args[i][2:]
+            if key in ["item_encoding", "seq_encoding"]: 
+                continue  
+            value = extra_args[i + 1]
+            try:
+                if '[' in value or '{' in value:
+                    # added for text_keys? 
+                    if "\\" in value: 
+                        value = value.replace("\\", "")
+                    value = json.loads(value)
+                    if isinstance(value, dict):
+                        for k, v in value.items():
+                            value[k] = convert_str(v)
+                    else:
+                        value = [convert_str(x) for x in value]
+                else:
+                    value = convert_str(value)
+                if '.' in key:
+                    k1, k2 = key.split('.')
+                    config[k1][k2] = value
+                else:
+                    config[key] = value
+            except:
+                raise ValueError(f"{key} {value} invalid")
+
+    init_seed(config['seed'], config['reproducibility'])
+    
+    # get model 
+    print("getting model with no dataload...")
+    model = get_model(config['model'])(config, None).to(device)
+    trainer = Trainer(config, model) # use for easy mixed precision setup
+
+
+    # load ckpt 
+    ckpt_path = os.path.join(config['checkpoint_dir'], 'pytorch_model.bin')
+    if os.path.exists(ckpt_path):
+        print(f"Found checkpoint at {ckpt_path}, attempting to load...")
+        ckpt = torch.load(ckpt_path, map_location='cpu')
+        # modify the word embedding -> get tops 
+        msg = trainer.model.load_state_dict(ckpt, strict=False)
+        print(f'Checkpoint loaded from {ckpt_path}')
+        print(f'{msg.unexpected_keys = }')
+        print(f'{msg.missing_keys = }')
+    else:
+        print("No checkpoint found. Starting training from scratch.")
+
+
+    if config['strategy'] == 'deepspeed':
+        print(f"Use deepspeed strategy")
+        precision = config['precision'] if config['precision'] else '32'
+        strategy = DeepSpeedStrategy(stage=config['stage'], precision=precision)
+        lite = L.Fabric(accelerator='gpu', strategy=strategy, precision=precision, num_nodes=1)
+        lite.launch()
+        model, optimizer = lite.setup(trainer.model, trainer.optimizer)
+    else:
+        print(f"Use DDP strategy")
+        precision = config['precision'] if config['precision'] else '32'
+        strategy = DDPStrategy(find_unused_parameters=True)
+        lite = L.Fabric(accelerator='gpu', strategy=strategy, precision=precision, num_nodes=1)
+        lite.launch()
+        model = lite.setup(traier.model)
+
+
+    del trainer
+    if optimizer: 
+        del optimizer
+
+
+    # encoding 
+    if custom_args.item_encoding: 
+        item_data = None
+        item_encode(model, config, custom_args, custom_args.output_path, save_step=custom_args.save_step) 
+    elif custom_args.seq_encoding: 
+        seq_data = None
+        test_seq_dataloader = DataLoader(
+            seq_data,
+            sampler=test_item_sampler,
+            batch_size=custom_args.batch_size,
+            drop_last=False,
+            num_workers=0,
+            collate_fn=seq_data.collect_fn
+        )
+        seq_encode(model, test_seq_dataloader, custom_args.output_path, save_step=custom_args.save_step)     
+
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config_file", nargs='+', type=str) # HLLM.yaml
+    args, extra_args = parser.parse_known_args()
+
+    custom_parser = argparse.ArgumentParser()
+    custom_parser.add_argument("--id_map_path", type=str, help="cwid_id mapping")
+    custom_parser.add_argument("--item_encoding", action="store_true", default=None) 
+    custom_parser.add_argument("--seq_encoding", action="store_true", default=None) 
+    custom_parser.add_argument("--best_model_path", type=str, help="Path of the best checkpoint")
+    custom_parser.add_argument("--output_path", type=str, help="Path to store output embeddings")
+    custom_parser.add_argument("--batch_size", type=int, default=32, help="Set batch size")
+    custom_parser.add_argument("--dataset_number_of_shards", type=int, default=1, help="Number of encoding shards")
+    custom_parser.add_argument("--dataset_shard_index", type=int, default=0, help="Index of current shard")
+    custom_parser.add_argument("--save_step", type=int, default=50, help="Number of batches to perform checkpointing")
+    custom_parser.add_argument("--item_size", type=int, default=32, help="Set item token length")
+    custom_parser.add_argument("--num_workers", type=int, default=1, help="Number of CPUs available for data processing")
+    custom_args, _ = custom_parser.parse_known_args()
+
+    config_file = args.config_file
+
+
+    local_rank = int(os.environ['LOCAL_RANK'])
+    torch.cuda.set_device(local_rank) # single GPU per shard 
+    dist.init_process_group(backend='nccl')
+
+    run(config_file=config_file, extra_args=extra_args, custom_args=custom_args)
---
+++ reproduce/HLLM_item_encode.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+#SBATCH --job-name=HLLM-encode
+#SBATCH --output=logs/%x-%j.out
+#SBATCH --error=logs/%x-%j.err
+
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=12
+
+#SBATCH --partition=general     
+#SBATCH --mem=128G 
+#SBATCH --gres=gpu:A6000:1
+
+#SBATCH --time=48:00:00
+
+#SBATCH --mail-type=END
+#SBATCH --mail-user="jingyuah@cs.cmu.edu"
+
+echo "Job Starts"
+
+eval "$(conda shell.bash hook)"
+conda activate hllm
+
+echo "activated"
+
+user_pretrain_dir="/data/user_data/jingyuah/HLLM_weights/checkpoints/TinyLlama-1.1B-Chat-v0.4"
+item_pretrain_dir="/data/user_data/jingyuah/HLLM_weights/checkpoints/TinyLlama-1.1B-Chat-v0.4"
+
+# checkpoint_dir="/data/user_data/jingyuah/HLLM_weights/checkpoints/repro_tiny_llama_1.1b_pixelrec_200K"
+checkpoint_dir="/data/group_data/cx_group/REC/checkpoints/HLLM-ml-1m"
+
+inter_path="/data/user_data/jingyuah/HLLM_weights/data/dataset"
+info_path="/data/user_data/jingyuah/HLLM_weights/data/information"
+
+epoch=5
+
+num_shards=128 # total number of shards 
+shard=$1 # the current shard, measuring sequentially 
+item_batch_size=256
+num_cpus=12 # more cpus -> fetch cw topics etc faster
+save_step=50 # towards how many steps do you save a checkpoint 
+
+echo $shard 
+
+# Clueweb id map path
+id_map_path="/data/group_data/cx_group/REC/ClueWeb-Reco/ClueWeb-Reco_public/cwid_to_id.tsv"
+embedding_output_dir="/data/group_data/cx_group/REC/ClueWeb-Reco/HLLM_exps/HLLM_item_encoding_ml-1m"
+
+
+export LOCAL_RANK=0
+export RANK=0
+export WORLD_SIZE=1
+export MASTER_ADDR=localhost
+export MASTER_PORT=$(expr 10000 + $(echo -n $SLURM_JOBID | tail -c 4))
+
+
+# Item and User LLM are initialized by specific pretrain_dir.
+python3 HLLM/code/encode.py \
+    --config_file HLLM/code/overall/LLM_deepspeed.yaml HLLM/code/HLLM/HLLM.yaml \
+    --loss nce \
+    --epochs $epoch \
+    --train_batch_size 8 \
+    --MAX_TEXT_LENGTH 256 \
+    --MAX_ITEM_LIST_LENGTH 10 \
+    --checkpoint_dir $checkpoint_dir \
+    --optim_args.learning_rate 1e-4 \
+    --item_pretrain_dir $item_pretrain_dir \
+    --user_pretrain_dir $user_pretrain_dir \
+    --text_path $info_path \
+    --data_path $inter_path \
+    --text_keys '[\"title\",\"tag\",\"description\"]'  \
+    --best_model_path $item_pretrain_dir \
+    --id_map_path $id_map_path \
+    --output_path $embedding_output_dir/clueweb-b-en.${shard}-of-${num_shards}.pkl \
+    --batch_size $item_batch_size \
+    --item_size 256 \
+    --seq_size 256 \
+    --dataset_number_of_shards $num_shards \
+    --dataset_shard_index $shard \
+    --save_step $save_step \
+    --num_workers $num_cpus \
+    --item_encoding 
+
+
+echo "Job Ends"
